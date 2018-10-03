e307543720ebeab4eb4352d196057a62eb73735c
Åke Forslund <ake.forslund@gmail.com>
2018-10-03 20:26:05 +0200

    Make main importable
    - create main-function
    - fix incorrect imports
    - correct syntax errors

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from glob import glob
 from datetime import date
 
 import polib
-from github_actions import get_work_repos, get_work_dir, create_pr
+from github_actions import get_work_repos, create_work_dir, create_or_edit_pr
 
 
 def download_lang(lang):
@@ -49,66 +49,65 @@ def insert_translation(path, translation):
 
 
 
+def main():
+    for lang in languages:
+        po_dir = get_language(lang)
+
+        # for all po files
+        # use glob to get all po-files in the directory
+        for f in glob(join(po_dir, '*')):
+            # TODO: get skill url for filename
 
 
-for lang in languages:
-    po_dir = get_language(lang)
+            print('Processing {}'.format(f))
+            translation = parse_po_file(f)
+            fork, upstream = get_work_repos(skill_url)
+            work = get_work_dir(upstream, fork)
 
-    # for all po files
-    # use glob to get all po-files in the directory
-    for f in glob(join(po_dir, '*')):
-        # TODO: get skill url for filename
+            # Modify repo
+            # Checkout new branch
+            branch = 'translate-' + str(date.today())
+            work.checkout('-b', branch)
 
-
-        print('Processing {}'.format(f))
-        translation = parse_po_file(f)
-        fork, upstream = get_work_repos(skill_url)
-        work = get_work_dir(upstream, fork)
-
-        # Modify repo
-        # Checkout new branch
-        branch = 'translate-' + str(date.today())
-        work.checkout('-b', branch)
-
-        if 'locale' in os.listdir('tmp_repo_path'):
-            path = join('locale', lang)
-            work.rm(join(path, '*'))  # Remove all files
-            # insert new translations
-            insert_translations(join(work.tmp_path, path), translations)
-            work.add(join(path, '*'))  # add the new files
-        else:
-            # handle dialog directory
-            path = join('dialog', lang)
-            work.rm(join(path, '*'))  # Remove all files
-            insert_translation(work, join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.dialog'})
-            insert_translation(join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.list'})
-            work.add(join(path, '*'))  # add the new files
-            # handle vocab directory
-            path = join('vocab', lang)
-            work.rm(join(path, '*'))  # Remove all files
-            insert_translation(join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.intent'})
-            insert_translation(work, join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.voc'})
-            insert_translation(join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.entity'})
-            work.add(join(path, '*'))  # add the new files
-            # Handle regex dir
-            path = join('regex', lang)
-            work.rm(join(path, '*'))  # Remove all files
-            insert_translation(join(work.tmp_path, path),
-                {k: translation[k] for k in translation if
-                    translation[k].endswith('.rx'})
-        # Commit
-        work.commit('-m', 'Update translations')
-        # Push branch to fork
-        work.push('work', branch)
-        # Open PR
-        create_or_edit_pr(branch, upstream, lang)
+            if 'locale' in os.listdir('tmp_repo_path'):
+                path = join('locale', lang)
+                work.rm(join(path, '*'))  # Remove all files
+                # insert new translations
+                insert_translations(join(work.tmp_path, path), translations)
+                work.add(join(path, '*'))  # add the new files
+            else:
+                # handle dialog directory
+                path = join('dialog', lang)
+                work.rm(join(path, '*'))  # Remove all files
+                insert_translation(work, join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.dialog')})
+                insert_translation(join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.list')})
+                work.add(join(path, '*'))  # add the new files
+                # handle vocab directory
+                path = join('vocab', lang)
+                work.rm(join(path, '*'))  # Remove all files
+                insert_translation(join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.intent')})
+                insert_translation(work, join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.voc')})
+                insert_translation(join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.entity')})
+                work.add(join(path, '*'))  # add the new files
+                # Handle regex dir
+                path = join('regex', lang)
+                work.rm(join(path, '*'))  # Remove all files
+                insert_translation(join(work.tmp_path, path),
+                    {k: translation[k] for k in translation if
+                        translation[k].endswith('.rx')})
+            # Commit
+            work.commit('-m', 'Update translations')
+            # Push branch to fork
+            work.push('work', branch)
+            # Open PR
+            create_or_edit_pr(branch, upstream, lang)
